bea99f2478ab8f8fcdf1a101d1d1cfc89f9be34d
joseomarojeda <106188117+joseomarojeda@users.noreply.github.com>
2022-12-03 23:23:04 -0600

    .

---
+++ Write-Tag.py
@@ -0,0 +1,13 @@
+import RPi.GPIO as GPIO
+from mfrc522 import SimpleMFRC522
+
+reader = SimpleMFRC522()
+
+try:
+        text = input('Ingresa Texto:')
+        print("Coloca tu tag cerca del lector")
+        reader.write(text)
+        print("Written")
+finally:
+        GPIO.cleanup()
+Footer
