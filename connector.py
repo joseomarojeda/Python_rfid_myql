3701282f8f4296eeee03345ee3a7d003e54e7af0
joseomarojeda <106188117+joseomarojeda@users.noreply.github.com>
2022-12-03 23:08:45 -0600

    .

--- connector.py
+++
@@ -1,6 +0,0 @@
-from mysql.connector import (connection)
-
-cnx = connection.MySQLConnection(user='omaro', password='1234',
-                                 host='127.1668.100.7',
-                                 database='codigoIoT')
-cnx.close()
--- python_connector.py
+++ python_connector.py
@@ -3,10 +3,9 @@ import mysql.connector
 
 # Programa
 print ("Conectandose a la base de datos")
-cnx = mysql.connector.connect(user='omaro',
-                              password='1234',
-                              host='192.168.100.7',
-                              database='codigoIoT')
+cnx = mysql.connector.connect(user='omarojeda', password='2602',
+                            host='192.168.100.7',
+                            database='codigoIoT')
 print ("Conexion realizada")
 print (cnx)
 
