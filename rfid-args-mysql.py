c214f0acd211dd23fb0a3c09f9f12e9c6cb6986f
joseomarojeda <106188117+joseomarojeda@users.noreply.github.com>
2022-12-03 23:22:16 -0600

    actualizar

---
+++ rfid-args-mysql.py
@@ -0,0 +1,51 @@
+# Bibliotecas
+from time import sleep
+import sys
+from mfrc522 import SimpleMFRC522
+import RPi.GPIO as GPIO
+import mysql.connector
+import argparse
+
+# Parser
+parser = argparse.ArgumentParser()
+parser.add_argument("status")
+args = parser.parse_args()
+
+# Inicar el sensor
+reader = SimpleMFRC522()
+
+# Conexión
+cnx = mysql.connector.connect(user='omarojeda', password='2602', host='192.168.100.7', database='codigoIoT')
+# Cursor
+cursor = cnx.cursor()
+
+# Cuerpo del programa
+try:
+    # Lectura unica
+    id, text = reader.read()
+    # print("ID: %s\nText: %s" % (id,text))
+    strrr = text.split(",")
+    # print (strrr)
+    # print (strrr[0])
+    sleep(1)
+    query_insert = "INSERT INTO rfid (nombre,texto,rfid) VALUES ('" + strrr[0] + "','" + args.status + "'," + str (id) + ");"
+    print (query_insert)
+    
+    # Ejecutar cursor
+    cursor.execute (query_insert)
+    # Asegurarse de realizar la operacion en BD
+    cnx.commit()
+    print ("Query Ok")
+    sleep (1)
+
+    # Cerrar
+    cursor.close()
+    cnx.close()
+    GPIO.cleanup()
+    
+except KeyboardInterrupt:
+    # Cerrar
+    cursor.close()
+    cnx.close()
+    GPIO.cleanup()
+    raise
