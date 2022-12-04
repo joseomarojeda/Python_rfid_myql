aa3c69c8dff18a0b975f08561c181b7587270545
joseomarojeda <106188117+joseomarojeda@users.noreply.github.com>
2022-12-03 18:43:36 -0600

    read_tag

---
+++ read_tag.py
@@ -0,0 +1,20 @@
+# Bibliotecas
+from time import sleep
+import sys
+from mfrc522 import SimpleMFRC522
+import RPi.GPIO as GPIO
+
+# Inicar el sensor
+reader = SimpleMFRC522()
+
+# Cuerpo del programa
+try:
+    # Leer el tag
+    while True:
+        print("Acercar el tag al lector")
+        id, text = reader.read()
+        print("ID: %s\nText: %s" % (id,text))
+        sleep(5)
+except KeyboardInterrupt:
+    GPIO.cleanup()
+    raise
